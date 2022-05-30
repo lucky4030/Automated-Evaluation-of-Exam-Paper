c475b4fff9cbc90c58c3425372d6bd9e6d81e6f1
satyendra tiwari <satyendra.tiwari@shiprocket.com>
2022-05-30 12:32:55 +0530

    added spinner on api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { sample, sampleTime } from 'rxjs';
 import axios from 'axios';
+import { SpinnerService } from './spinner/spinner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   
+  constructor( private spinnerService : SpinnerService ) { }
+
   result:number = 0;
   resultStatus :boolean = false;
   sampleAnswer = '';
   testAnswer = '';
-
+  //https://medium.com/tech-insights/how-to-deploy-angular-apps-to-github-pages-gh-pages-896c4e10f9b4
+  // for deployment!
   payload : object = { 'sample' :'', 'test' : ''};
 
   setSample( vari : any ){
@@ -29,7 +34,10 @@ export class AppComponent {
   onClickCond(  ){
     if( this.sampleAnswer == '' ) alert('Please enter sample Answer !');
     else{
+      
       this.payload = { 'sample': this.sampleAnswer, 'test': this.testAnswer };
+      
+      this.spinnerService.requestStarted();
 
       axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
       axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
@@ -38,10 +46,11 @@ export class AppComponent {
           console.log(response) 
           this.resultStatus = true
           this.result = response['data'] * 100
-          
+          this.spinnerService.requestEnded();
         })
       .catch(error => {
           // element.parentElement.innerHTML = `Error: ${error.message}`;
+          this.spinnerService.resetSpinner();
           console.error('There was an error!', error);
       });
       console.log( this.payload );
